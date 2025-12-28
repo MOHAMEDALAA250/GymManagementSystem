63c14d7c69f572bc16db473c631ebdbcc7732ad9
MOHAMED ALAA <mohamednalaa12@gmail.com>
2025-12-28 03:25:18 +0200

    Core Entities

---
+++ GymManagementDAL/Entities/Member.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Entities
+{
+    internal class Member : GymUser
+    {
+        public string? Photo { get; set; }
+    }
+}
---
+++ GymManagementDAL/Entities/Trainer.cs
@@ -0,0 +1,15 @@
+﻿using GymManagementDAL.Entities.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Entities
+{
+    public class Trainer : GymUser
+    {
+        public Specialties Specialties { get; set; }
+    }
+
+}
